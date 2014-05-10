a559ed3b3cb4bf372a1e7ffa91c29aab4039357f
Ikuo Matsumura <makiczar@gmail.com>
2014-05-11 00:54:02 +0900

    Display version name and version code

--- src/main/scala/fragment/AboutFragment.scala
+++ src/main/scala/fragment/AboutFragment.scala
@@ -1,15 +1,29 @@
 package com.github.ikuo.garaponoid
 
 import android.os.Bundle
+import android.os.Build.VERSION
 import android.app.{Activity, Fragment}
 import android.view.{View, ViewGroup, LayoutInflater}
+import android.widget.TextView
+import TypedResource._
 
 class AboutFragment extends Fragment {
+  lazy val versionName = packageInfo.versionName
+  lazy val versionCode = packageInfo.versionCode
+  lazy val packageInfo = {
+    val context = getActivity
+    context.getPackageManager.
+      getPackageInfo(context.getPackageName, 0)
+  }
+
   override def onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup,
     savedInstanceState: Bundle
   ): View = {
-    inflater.inflate(R.layout.about_view, container, false)
+    val view = inflater.inflate(R.layout.about_view, container, false)
+    view.findView[TextView](TR.version_name).setText(versionName)
+    view.findView[TextView](TR.version_code).setText(s"(#${versionCode})")
+    view
   }
 }
